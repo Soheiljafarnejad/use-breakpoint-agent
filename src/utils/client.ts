2af40f991807a2175ba1f6357a859334a5e17929
Soheil <jafarnejad.official@gmail.com>
2025-06-05 00:42:40 +0330

    fix: 🐛 Added default Input in getDeviceTypeFromString

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -6,7 +6,7 @@ export function getDeviceTypeFromString(input: string | undefined): DeviceEnum {
     throw new Error("use-breakpoint-agent/client cannot be used on the server side");
   }
 
-  const data = UAParser(input);
+  const data = UAParser(input || navigator.userAgent);
   const device = data.device?.type;
 
   if (device === "mobile") return DeviceEnum.MOBILE;
